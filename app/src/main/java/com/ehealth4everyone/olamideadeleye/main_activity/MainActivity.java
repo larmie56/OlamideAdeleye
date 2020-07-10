aa02b63bbeba393ae33578046547b474717185ed
larmie56 <adeleyeolamide56@gmail.com>
2020-07-10 03:19:57 +0100

    Refactor MainActivity

--- app/src/main/java/com/ehealth4everyone/olamideadeleye/main_activity/MainActivity.java
+++ app/src/main/java/com/ehealth4everyone/olamideadeleye/main_activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.ehealth4everyone.olamideadeleye;
+package com.ehealth4everyone.olamideadeleye.main_activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.ehealth4everyone.olamideadeleye.R;
 
 public class MainActivity extends AppCompatActivity {
 
